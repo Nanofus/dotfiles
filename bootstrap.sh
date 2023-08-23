2df5a885e5f81bff978a8f4c349ead35d6e3fce1
Ville Talonpoika <ville@talonpoika.net>
2023-08-23 12:05:42 +0300

    Test

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,21 @@
 # Install OS-specific configuration
 
-cp .base.gitconfig .gitconfig
+cp ~/.base.gitconfig ~/.gitconfig
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  source .macos.bootstrap.config
-  cp .base.hyper.json .config/Hyper/hyper.json
-  sed -i "s|HYPER_SHELL|${HYPER_SHELL}|" .config/Hyper/hyper.json
+  source ~/.macos.bootstrap.config
+  cp ~/.base.hyper.json ~/.config/Hyper/hyper.json
+  sed -i '' -e "s|HYPER_SHELL|${HYPER_SHELL}|" ~/.config/Hyper/hyper.json
+  sed -i '' -e "s|GIT_AUTOCRLF|${GIT_AUTOCRLF}|" ~/.gitconfig
+  sed -i '' -e "s|GIT_SSH_COMMAND|${GIT_SSH_COMMAND}|" ~/.gitconfig
+  sed -i '' -e "s|GIT_SSH_PROGRAM|${GIT_SSH_PROGRAM}|" ~/.gitconfig
 elif [[ "$OSTYPE" == "msys" ]]; then
-  source .windows.bootstrap.config
-  cp .base.hyper.json $APPDATA/Hyper/hyper.json
+  source ~/.windows.bootstrap.config
+  cp ~/.base.hyper.json $APPDATA/Hyper/hyper.json
   sed -i "s|HYPER_SHELL|${HYPER_SHELL}|" $APPDATA/Hyper/hyper.json
+  sed -i "s|GIT_AUTOCRLF|${GIT_AUTOCRLF}|" ~/.gitconfig
+  sed -i "s|GIT_SSH_COMMAND|${GIT_SSH_COMMAND}|" ~/.gitconfig
+  sed -i "s|GIT_SSH_PROGRAM|${GIT_SSH_PROGRAM}|" ~/.gitconfig
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "not implemented"
 fi
-
-sed -i "s|GIT_AUTOCRLF|${GIT_AUTOCRLF}|" .gitconfig
-sed -i "s|GIT_SSH_COMMAND|${GIT_SSH_COMMAND}|" .gitconfig
-sed -i "s|GIT_SSH_PROGRAM|${GIT_SSH_PROGRAM}|" .gitconfig
