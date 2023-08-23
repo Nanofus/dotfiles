5a22206c80e048cb8181eaf7da70ca47a5fe5a99
Ville Talonpoika <ville@talonpoika.net>
2023-08-23 12:33:30 +0300

    Update converge

--- converge.sh
+++ converge.sh
@@ -3,16 +3,16 @@
 cp .gitconfig .base.gitconfig
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  source .macos.bootstrap.config
-  cp .config/Hyper/hyper.json .base.hyper.json
+  source ~/.macos.bootstrap.config
+  cp ~/.config/Hyper/hyper.json ~/.base.hyper.json
 elif [[ "$OSTYPE" == "msys" ]]; then
-  source .windows.bootstrap.config
-  cp $APPDATA/Hyper/hyper.json .base.hyper.json
+  source ~/.windows.bootstrap.config
+  cp $APPDATA/Hyper/hyper.json ~/.base.hyper.json
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "not implemented"
 fi
 
-sed -i "s|${HYPER_SHELL}|HYPER_SHELL|" .base.hyper.json
-sed -i "s|${GIT_AUTOCRLF}|GIT_AUTOCRLF|" .base.gitconfig
-sed -i "s|${GIT_SSH_COMMAND}|GIT_SSH_COMMAND|" .base.gitconfig
-sed -i "s|${GIT_SSH_PROGRAM}|GIT_SSH_PROGRAM|" .base.gitconfig
+sed -i '' -e "s|${HYPER_SHELL}|HYPER_SHELL|" ~/.base.hyper.json
+sed -i '' -e "s|${GIT_AUTOCRLF}|GIT_AUTOCRLF|" ~/.base.gitconfig
+sed -i '' -e "s|${GIT_SSH_COMMAND}|GIT_SSH_COMMAND|" ~/.base.gitconfig
+sed -i '' -e "s|${GIT_SSH_PROGRAM}|GIT_SSH_PROGRAM|" ~/.base.gitconfig
